044eb8f89980d482f4b3716e3da88cd6c8b98c65
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-09-07 06:35:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main (void)
+{
+	puts ("\ "Programming is like building a multilingual puzzle");
+	return(0);
+}
