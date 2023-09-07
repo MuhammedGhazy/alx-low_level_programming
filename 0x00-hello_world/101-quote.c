5fc8fe02b9f2226fbde0d76a8ac48d8193ed8ad1
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-09-07 15:28:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: Muhammed Advance Task "UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity"
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (i);
