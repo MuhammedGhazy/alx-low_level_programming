25803d62165b881c2aa37f04b5dc9e6e739e010a
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-09-12 12:43:45 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,13 @@
  *
 */
 int main(void)
+{
 	char str[] = "_putchar";
 	int ch;
 
 	for(ch = 0; ch < 8; ch++)
-		_putchar(str[ch])
-		_putchar('\n');
+		_putchar(str[ch]);
+	_putchar('\n');
 
 		return (0);
+}
