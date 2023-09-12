644203f07cdf4596a1e2b544a8ea9c575871c655
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-09-12 15:54:36 -0700

    6-

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+ * _abs - function that computes the absalute
+ *	value of an int
+ *
+ * @n: takes in int type input for fun
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
