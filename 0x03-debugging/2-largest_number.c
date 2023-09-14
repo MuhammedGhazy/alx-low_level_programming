497733fd2fa6a926f3be917f122c632f2ad79f79
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-09-14 08:54:12 -0700

    2-largest_number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
@@ -27,4 +27,3 @@ largest = c;
 
 return (largest);
 }
-~            
