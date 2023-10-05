9a9782ce2b3a6bf3ff5f99ac9ff49bab98fe0af4
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-10-04 17:49:07 -0700

    0x0B-malloc_free/

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *create_array - function
+ *
+ * @size: input for size
+ *
+ * @c: input char
+ *
+ * Return: pointer
+ *
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+
+	while (size--)
+		n[size] = c;
+	return (n);
+}
