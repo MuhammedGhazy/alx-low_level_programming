89541c39c2629540668c3559d9f9024a78074223
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-10-07 08:19:37 -0700

    2-calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory
+ *
+ * @s: pointer
+ * @b: constant
+ * @n: max byte(s)
+ *
+ * Return: S
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+
+/**
+ * *_calloc - function allocated memory
+ *
+ * @nmemb: array length
+ * @size: size of each element
+ *
+ * Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+	return (m);
+}
