3b4a296024be97dfdb074a1a6e6bfb0ecc1a1227
MuhammedGhazy <Ghazymuhammed00@gmail.com>
2023-10-07 07:51:34 -0700

    1-string_nconca

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_nconcat - function
+ *
+ * @s1: input pointer
+ * @s2: input pointer
+ * @n: number of byte(s)
+ *
+ * Return: pointer
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, co, s1_l, s2_l;
+
+	/*check if the strings passed are null*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	/*compute the length of the string*/
+	for (s1_l = 0; s1[s1_l] != '\0'; s1_l++)
+		;
+	for (s2_l = 0; s2[s2_l] != '\0'; s2_l++)
+		;
+	/* memory reservation-for case 1 & 2*/
+	str = malloc(s1_l + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/*copy first string into str.*/
+	for (co = 0; co < n; co++)
+	{
+		str[i] = s2[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
