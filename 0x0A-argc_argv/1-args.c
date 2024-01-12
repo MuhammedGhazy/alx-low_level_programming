d94b7c9bdf26c39079802701d48db47a3448ab4f
Muhammed Ghazy <ghazymuhammed00@gmail.com>
2024-01-12 06:05:17 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - func print the number of argument passed to programe, followed by a new line
+ *
+ * @argc: number of argument
+ *
+ * @argv: array of argument
+ *
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+        (void)argv;
+        printf("%i\n", argc - 1);
+        return (0);
+}
